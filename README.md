b10b6169610588bb0c4762f963002b9b9205a719
Brian Wyant <brianwyant@curalate.com>
2017-12-03 14:50:11 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Generated from the react-static-boilerplate; license at https://github.com/kriasoft/react-static-boilerplate/blob/master/LICENSE.txt
--- src/actions/urls.js
+++
@@ -1,7 +0,0 @@
-
-export const homescoreUrl = (userId) =>
-  `https://stg-garcon.herokuapp.com/api/1/subscriptions/homescore/${userId}/summary`;
-
-export const dailyUsageUrl = (userId) =>
-  `https://stg-garcon.herokuapp.com/api/1/subscriptions/homescore/${userId}/energy/usage/daily`
-
